c53d89d8d166efc26a440335bc36d61574baed96
Ben <bkmi@users.noreply.github.com>
2025-03-14 21:16:40 +0000

    inckpt

--- data/llm_osc/join_preprocess.sh
+++ data/llm_osc/join_preprocess.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pickles=(/home/bkmi/flowmm/data/llm_osc/train*.pkl)
-python /home/bkmi/flowmm/src/flowmm/common/combine_manual_preprocessed.py /home/bkmi/flowmm/data/llm_osc/train_ori.pt --pickles ${pickles[@]}
-python /home/bkmi/flowmm/src/flowmm/common/combine_manual_preprocessed.py /home/bkmi/flowmm/data/llm_osc/val_ori.pt --pickles /home/bkmi/flowmm/data/llm_osc/val_t1.5.pkl
+pickles=(/fsx-open-catalyst/bkmi/flowmm/data/llm_osc/train*.pkl)
+python /fsx-open-catalyst/bkmi/flowmm/src/flowmm/common/combine_manual_preprocessed.py /fsx-open-catalyst/bkmi/flowmm/data/llm_osc/train_ori.pt --pickles ${pickles[@]}
+python /fsx-open-catalyst/bkmi/flowmm/src/flowmm/common/combine_manual_preprocessed.py /fsx-open-catalyst/bkmi/flowmm/data/llm_osc/val_ori.pt --pickles /fsx-open-catalyst/bkmi/flowmm/data/llm_osc/val_t1.5.pkl
